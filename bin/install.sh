5c6300cd070c582e8ca83f6b1ff3cdb3722ae43e
Loka <loka@iim.ai>
2024-04-05 01:05:55 +0800

    latest

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker pull iimeta/fastapi-admin:0.1.2
+docker pull iimeta/fastapi-admin:latest
 
 mkdir -p /data/fastapi-admin/manifest/config
 mkdir -p /data/fastapi-admin/resource/public
--- bin/start.sh
+++ bin/start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-docker run -d \
+docker run --name fastapi-admin -d -p 8080:8080 \
   --network host \
   --restart=always \
-  -p 8080:8080 \
   -v /etc/localtime:/etc/localtime:ro \
   -v /data/fastapi-admin/manifest/config/config.yaml:/app/manifest/config/config.yaml \
   -v /data/fastapi-admin/resource/public:/app/resource/public \
-  --name fastapi-admin \
-  iimeta/fastapi-admin:0.1.2
+  iimeta/fastapi-admin:latest
