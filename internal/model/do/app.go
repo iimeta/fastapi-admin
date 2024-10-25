5931c6a1afd00c4c246200e7b8d87922519ad2ca
Loka <loka@iim.ai>
2024-10-25 09:53:57 +0800

    删除无关代码

--- internal/model/do/app.go
+++ internal/model/do/app.go
@@ -15,8 +15,6 @@ type App struct {
 	Quota          int      `bson:"quota"`                // 剩余额度
 	UsedQuota      int      `bson:"used_quota,omitempty"` // 已用额度
 	QuotaExpiresAt int64    `bson:"quota_expires_at"`     // 额度过期时间
-	RPM            int      `bson:"rpm,omitempty"`        // 每分钟请求数
-	RPD            int      `bson:"rpd,omitempty"`        // 每天的请求数
 	IpWhitelist    []string `bson:"ip_whitelist"`         // IP白名单
 	IpBlacklist    []string `bson:"ip_blacklist"`         // IP黑名单
 	Remark         string   `bson:"remark"`               // 备注
