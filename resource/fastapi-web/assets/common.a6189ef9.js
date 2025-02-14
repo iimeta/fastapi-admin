8e7647625340dd697ee594bc48360085ee51cfaf
Loka <loka@iim.ai>
2025-02-14 22:25:10 +0800

    更新前端

---
+++ resource/fastapi-web/assets/common.a6189ef9.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index.46908301.js";function c(o){return a.post("/api/v1/common/email-code",o)}export{c as g};
--- resource/fastapi-web/assets/common.a642526b.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./index.ce77669f.js";function c(o){return a.post("/api/v1/common/email-code",o)}export{c as g};
