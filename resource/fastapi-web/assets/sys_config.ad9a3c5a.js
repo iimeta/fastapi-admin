004fd4c0c6aa013fe2f566257a784ec1ac582308
Loka <loka@iim.ai>
2025-02-22 17:18:56 +0800

    更新前端

--- resource/fastapi-web/assets/sys_config.ad9a3c5a.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./index.853b9bb5.js";function i(){return t.get("/api/v1/sys/config/detail")}function o(s){return t.post("/api/v1/sys/config/update",s)}function a(s){return t.post("/api/v1/sys/config/change/status",s)}function e(s){return t.post("/api/v1/sys/config/reset",s)}function u(){return t.get("/api/v1/open/sys/config")}export{e as a,a as b,i as c,u as q,o as s};
