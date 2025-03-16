496cc8646eeef5e6e080bb5ff20680bdf2a29c5c
Loka <loka@iim.ai>
2025-03-16 16:06:43 +0800

    用户管理完善, 新增邮箱字段, 详情优化

--- internal/model/user.go
+++ internal/model/user.go
@@ -35,6 +35,7 @@ type UserUpdateInfoReq struct {
 // 新建用户接口请求参数
 type UserCreateReq struct {
 	Name           string `json:"name,omitempty" v:"required"`                               // 姓名
+	Email          string `json:"email,omitempty" v:"required"`                              // 邮箱
 	Account        string `json:"account,omitempty" v:"required"`                            // 账号
 	Password       string `json:"password,omitempty" v:"required|min-length:6"`              // 密码
 	Terminal       string `json:"terminal,omitempty" v:"required|in:web,h5,ios,windows,mac"` // 终端
@@ -47,6 +48,7 @@ type UserCreateReq struct {
 type UserUpdateReq struct {
 	Id             string   `json:"id,omitempty"`                        // ID
 	Name           string   `json:"name,omitempty" v:"required"`         // 姓名
+	Email          string   `json:"email,omitempty" v:"required"`        // 邮箱
 	Account        string   `json:"account,omitempty" v:"required"`      // 账号
 	Password       string   `json:"password,omitempty" v:"min-length:6"` // 密码
 	Type           int      `json:"type,omitempty"`                      // 用户类型
@@ -131,28 +133,30 @@ type UserModelsReq struct {
 }
 
 type User struct {
-	Id                     string        `json:"id,omitempty"`                // ID
-	UserId                 int           `json:"user_id,omitempty"`           // 用户ID
-	Name                   string        `json:"name,omitempty"`              // 姓名
-	Avatar                 string        `json:"avatar,omitempty"`            // 头像
-	Email                  string        `json:"email,omitempty"`             // 邮箱
-	Phone                  string        `json:"phone,omitempty"`             // 手机号
-	Quota                  int           `json:"quota"`                       // 剩余额度
-	UsedQuota              int           `json:"used_quota"`                  // 已用额度
-	QuotaExpiresAt         string        `json:"quota_expires_at"`            // 额度过期时间
-	Models                 []string      `json:"models,omitempty"`            // 模型权限
-	ModelNames             []string      `json:"model_names,omitempty"`       // 模型名称
-	Account                string        `json:"account,omitempty"`           // 账号
-	QuotaWarning           bool          `json:"quota_warning,omitempty"`     // 额度预警
-	WarningThreshold       int           `json:"warning_threshold,omitempty"` // 预警阈值, 单位: $
-	ExpireWarningThreshold time.Duration `json:"expire_warning_threshold"`    // 过期预警阈值, 单位: 天
-	WarningNotice          bool          `json:"warning_notice,omitempty"`    // 预警通知
-	ExhaustionNotice       bool          `json:"exhaustion_notice,omitempty"` // 耗尽通知
-	Remark                 string        `json:"remark,omitempty"`            // 备注
-	Status                 int           `json:"status,omitempty"`            // 状态[1:正常, 2:禁用, -1:删除]
-	LoginIP                string        `json:"login_ip,omitempty"`          // 登录IP
-	LoginTime              string        `json:"login_time,omitempty"`        // 登录时间
-	LoginDomain            string        `json:"login_domain,omitempty"`      // 登录域名
-	CreatedAt              string        `json:"created_at,omitempty"`        // 创建时间
-	UpdatedAt              string        `json:"updated_at,omitempty"`        // 更新时间
+	Id                     string        `json:"id,omitempty"`                    // ID
+	UserId                 int           `json:"user_id,omitempty"`               // 用户ID
+	Name                   string        `json:"name,omitempty"`                  // 姓名
+	Avatar                 string        `json:"avatar,omitempty"`                // 头像
+	Email                  string        `json:"email,omitempty"`                 // 邮箱
+	Phone                  string        `json:"phone,omitempty"`                 // 手机号
+	Quota                  int           `json:"quota"`                           // 剩余额度
+	UsedQuota              int           `json:"used_quota"`                      // 已用额度
+	QuotaExpiresAt         string        `json:"quota_expires_at"`                // 额度过期时间
+	Models                 []string      `json:"models,omitempty"`                // 模型权限
+	ModelNames             []string      `json:"model_names,omitempty"`           // 模型名称
+	Account                string        `json:"account,omitempty"`               // 账号
+	QuotaWarning           bool          `json:"quota_warning,omitempty"`         // 额度预警
+	WarningThreshold       int           `json:"warning_threshold"`               // 预警阈值, 单位: $
+	ExpireWarningThreshold time.Duration `json:"expire_warning_threshold"`        // 过期预警阈值, 单位: 天
+	WarningNotice          bool          `json:"warning_notice,omitempty"`        // 预警通知
+	ExhaustionNotice       bool          `json:"exhaustion_notice,omitempty"`     // 耗尽通知
+	ExpireWarningNotice    bool          `json:"expire_warning_notice,omitempty"` // 额度过期预警通知
+	ExpireNotice           bool          `json:"expire_notice,omitempty"`         // 额度过期通知
+	Remark                 string        `json:"remark,omitempty"`                // 备注
+	Status                 int           `json:"status,omitempty"`                // 状态[1:正常, 2:禁用, -1:删除]
+	LoginIP                string        `json:"login_ip,omitempty"`              // 登录IP
+	LoginTime              string        `json:"login_time,omitempty"`            // 登录时间
+	LoginDomain            string        `json:"login_domain,omitempty"`          // 登录域名
+	CreatedAt              string        `json:"created_at,omitempty"`            // 创建时间
+	UpdatedAt              string        `json:"updated_at,omitempty"`            // 更新时间
 }
