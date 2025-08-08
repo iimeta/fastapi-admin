71fb0f92ee2c7bcc1b0686e9b0810b909a55d6a1
Loka <loka@iim.ai>
2025-08-08 17:35:00 +0800

    细节优化

--- internal/controller/group/group_v1_update.go
+++ internal/controller/group/group_v1_update.go
@@ -2,12 +2,15 @@ package group
 
 import (
 	"context"
+
 	"github.com/iimeta/fastapi-admin/internal/service"
 
 	"github.com/iimeta/fastapi-admin/api/group/v1"
 )
 
 func (c *ControllerV1) Update(ctx context.Context, req *v1.UpdateReq) (res *v1.UpdateRes, err error) {
+
+	req.Discount /= 100
 
 	err = service.Group().Update(ctx, req.GroupUpdateReq)
 
