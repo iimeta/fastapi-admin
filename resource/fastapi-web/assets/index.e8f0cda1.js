7a3ff920caee57eadc9b16fe2d13b65092e7ce39
Loka <loka@iim.ai>
2026-03-20 22:14:56 +0800

    v2.2.0

---
+++ resource/fastapi-web/assets/index.e8f0cda1.js
@@ -0,0 +1,1 @@
+import{d as o,B as r,C as a}from"./arco.a8ec1c81.js";import{f as s,h as n}from"./vue.c41ca615.js";import"./chart.3f86170c.js";const d=o({__name:"index",setup(p){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(r(),a("div"))}});export{d as default};
