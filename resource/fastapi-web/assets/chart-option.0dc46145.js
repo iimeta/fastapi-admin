f8d310fe79a3bedf9d0392ce27667702ba977d01
Loka <loka@iim.ai>
2026-07-11 20:44:25 +0800

    更新前端

--- resource/fastapi-web/assets/chart-option.0dc46145.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./arco.a816289b.js";import{b as n}from"./index.b0f1b639.js";function u(r){const o=n(),e=t(()=>o.theme==="dark");return{chartOption:t(()=>r(e.value))}}export{u};
--- resource/fastapi-web/assets/open.701c8634.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./index.b0f1b639.js";function a(e){return r.post("/api/v1/open/user/agreement",e)}function i(e){return r.post("/api/v1/open/privacy/policy",e)}export{i as a,a as q};
