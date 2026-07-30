e394e32f92d55241a8d372f7eb034668b54a43d8
Loka <loka@iim.ai>
2026-07-30 21:55:53 +0800

    更新前端

--- resource/fastapi-web/assets/monitor.a5ae8523.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./index.7f52f5c8.js";function n(o){return r.post("/api/v1/monitor/perf/breakdown",o)}function i(o){return r.post("/api/v1/monitor/perf/history",o)}function a(){return r.post("/api/v1/monitor/global")}export{n as a,i as b,a as q};
