040c7d744a3db9fb5d15abbf6355eb4906463391
Loka <loka@iim.ai>
2026-08-11 00:44:40 +0800

    更新前端

--- resource/fastapi-web/assets/open.fc1a0dd7.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./index.d1f6aae0.js";function a(e){return r.post("/api/v1/open/user/agreement",e)}function i(e){return r.post("/api/v1/open/privacy/policy",e)}export{i as a,a as q};
